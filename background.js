858b1c886522b3455c406dbb73d4cee7be161ab7
Louis-Marie Michelin <lmichelin@outlook.fr>
2019-10-11 15:42:54 +0200

    6. Log all requests in background script console

--- background.js
+++ background.js
@@ -1,1 +1,6 @@
-console.log("Hello from background script!")
+chrome.webRequest.onBeforeRequest.addListener(
+  requestDetails => {
+    console.log(requestDetails.url)
+  },
+  { urls: ["<all_urls>"] },
+)
