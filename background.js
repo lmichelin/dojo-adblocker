503c4773dadafb92e623ded6af315e8dd3fa5752
Louis-Marie Michelin <lmichelin@outlook.fr>
2019-10-11 15:53:52 +0200

    7. Fetch hosts list

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
+const hostsListUrl =
+  "https://block.energized.pro/spark/formats/domains.txt"
+
+fetch(hostsListUrl)
+  .then(response => response.text())
+  .then(responseText => {
+    let hostsToBlock = responseText.split("\n").filter(line => !line.startsWith("#"))
+    console.log(`${hostsToBlock.length} hosts will be blocked`)
+  })
+
 chrome.webRequest.onBeforeRequest.addListener(
   requestDetails => {
     console.log(requestDetails.url)
