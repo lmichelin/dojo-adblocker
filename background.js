a39abc301300c088e47a7544e21bab65ef9c1c91
Louis-Marie Michelin <lmichelin@outlook.fr>
2019-10-15 01:09:08 +0200

    9. Block ads from patterns list

--- background.js
+++ background.js
@@ -1,22 +1,35 @@
 const hostsListUrl =
   "https://block.energized.pro/spark/formats/domains.txt"
 
-fetch(hostsListUrl)
-  .then(response => response.text())
-  .then(responseText => {
-    let urlsToBlock = responseText
-      .split("\n")
-      .filter(line => !line.startsWith("#"))
-      .map(host => `*://${host}/*`)
+const patternsListUrl =
+  "https://raw.githubusercontent.com/easylist/easylist/master/easylist/easylist_general_block.txt"
 
-    console.log(`${urlsToBlock.length} hosts will be blocked`)
+const fetchListsAndEnableAdBlocking = async () => {
+  const hostsResponse = await fetch(hostsListUrl)
+  const hostsResponseText = await hostsResponse.text()
+  const hostsArray = hostsResponseText
+    .split("\n")
+    .filter(line => !line.startsWith("#"))
+    .map(host => `*://${host}/*`)
 
-    chrome.webRequest.onBeforeRequest.addListener(
-      requestDetails => {
-        console.log(requestDetails.url)
-        return { cancel: true }
-      },
-      { urls: urlsToBlock },
-      ["blocking"],
-    )
-  })
+  const patternsResponse = await fetch(patternsListUrl)
+  const patternsResponseText = await patternsResponse.text()
+  const patternsArray = patternsResponseText
+    .split("\n")
+    .filter(line => line !== "" && !line.startsWith("! "))
+    .map(url => `*://*/*${url}*`)
+
+  let urlsToBlock = hostsArray.concat(patternsArray)
+  console.log(`${urlsToBlock.length} hosts will be blocked`)
+
+  chrome.webRequest.onBeforeRequest.addListener(
+    requestDetails => {
+      console.log(requestDetails.url)
+      return { cancel: true }
+    },
+    { urls: urlsToBlock },
+    ["blocking"],
+  )
+}
+
+fetchListsAndEnableAdBlocking()
